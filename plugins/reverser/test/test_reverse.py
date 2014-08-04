19cbc0de3d2de342d10dc8e67c09eaaacede2e19
Sebastian Jansson <sebbz@sebbz.se>
2014-08-04 20:22:27 +0200

    Removed some unnecessary stuff

--- plugins/reverser/test/test_reverse.py
+++ plugins/reverser/test/test_reverse.py
@@ -8,9 +8,6 @@ from plugins.reverser.reverser import Reverser
 
 class FormatTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.dir = os.path.join("..", os.path.dirname(__file__))
-
     def test_reverse(self):
         message = Reverser._reverseString("sirap")
         self.assertEqual(message, "paris")
