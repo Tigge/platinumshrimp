bedc59176112cfe3806dec301977f0fc5f15f9d1
Sebastian Jansson <sebbz@sebbz.se>
2014-08-04 19:58:12 +0200

    The one reverser plugin, initial version

---
+++ plugins/reverser/reverser.py
@@ -0,0 +1,17 @@
+import plugin
+
+from twisted.python import log
+
+class Reverser(plugin.Plugin):
+
+    def __init__(self):
+        plugin.Plugin.__init__(self, "Reverser")
+
+    def _reverseString(self, text):
+        return text[::-1]
+
+    def privmsg(self, server_id, user, channel, message):
+        if message[:8] == ".reverse":
+            self.say(server_id, channel, self._reverseString(message[8:]))
+
+Reverser.run()
