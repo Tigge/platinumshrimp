f75e7b8f4058665d799a0609ae008d7cc5e30b33
Henrik Steen <reggna+github@reggna.se>
2014-10-25 12:28:29 +0200

    Fix the reverser plugin and its unit test

--- plugins/reverser/reverser.py
+++ plugins/reverser/reverser.py
@@ -7,11 +7,13 @@ class Reverser(plugin.Plugin):
     def __init__(self):
         plugin.Plugin.__init__(self, "Reverser")
 
-    def _reverseString(self, text):
+    @staticmethod
+    def _reverseString(text):
         return text[::-1]
 
     def privmsg(self, server_id, user, channel, message):
         if message.startswith(".reverse"):
-            self.say(server_id, channel, self._reverseString(message[8:]))
+            self.say(server_id, channel, _reverseString(message[8:]))
 
-Reverser.run()
+if __name__ == "__main__":
+    sys.exit(Reverser.run())
