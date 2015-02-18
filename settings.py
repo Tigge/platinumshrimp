b87090ae96f53dbd831d19e7ccd15334139026da
Henrik Steen <reggna+github@reggna.se>
2015-02-18 13:57:49 -0800

    Prettify default settings JSON

--- settings.py
+++ settings.py
@@ -34,9 +34,7 @@ def get_settings(settings_file=DEFAULT_SETTINGS_FILE):
                         'chalmersit': ['#platinumshrimp']
                       }
                     }]
-                }))
-            # TODO: Prettify output
-
+                }, indent=2))
     def encode_dict(data):
         if type(data) == dict:
             return dict(map(encode_dict, pair) for pair in data.items())
