f070e73bdd03aa1fafebc4fddbafaba3512d767e
Gustav Tiger <gustav@tiger.name>
2015-09-16 00:04:20 +0200

    NBT: Date util
    Changes:
    
    - Moved to Python 3
    - Remove twisted
    - Moved to irc (https://bitbucket.org/jaraco/irc)
    - PEP8 Fixes

--- utils/test/test_date_utils.py
+++ utils/test/test_date_utils.py
@@ -1,8 +1,7 @@
 from __future__ import division, absolute_import, print_function, unicode_literals
 
 import datetime
-
-from twisted.trial import unittest
+import unittest
 
 from utils import date_utils
 
@@ -11,37 +10,37 @@ class TestDateUtils(unittest.TestCase):
 
     def test_seconds(self):
 
-        date_old = datetime.datetime(2009, 02, 01, 16, 35, 23)
-        date_new = datetime.datetime(2009, 02, 01, 16, 35, 25)
+        date_old = datetime.datetime(2009, 2, 1, 16, 35, 23)
+        date_new = datetime.datetime(2009, 2, 1, 16, 35, 25)
         string = date_utils.format(date_old, date_new)
         self.assertEqual(string, "2 seconds ago")
 
     def test_minutes(self):
-        date_old = datetime.datetime(2009, 02, 01, 16, 35, 23)
-        date_new = datetime.datetime(2009, 02, 01, 16, 38, 30)
+        date_old = datetime.datetime(2009, 2, 1, 16, 35, 23)
+        date_new = datetime.datetime(2009, 2, 1, 16, 38, 30)
         string = date_utils.format(date_old, date_new)
         self.assertEqual(string, "3 minutes ago")
 
     def test_hours(self):
-        date_old = datetime.datetime(2009, 02, 01, 16, 35, 23)
-        date_new = datetime.datetime(2009, 02, 01, 17, 38, 30)
+        date_old = datetime.datetime(2009, 2, 1, 16, 35, 23)
+        date_new = datetime.datetime(2009, 2, 1, 17, 38, 30)
         string = date_utils.format(date_old, date_new)
         self.assertEqual(string, "1 hour ago")
 
     def test_days(self):
-        date_old = datetime.datetime(2009, 02, 01, 16, 35, 23)
-        date_new = datetime.datetime(2009, 02, 05, 17, 38, 30)
+        date_old = datetime.datetime(2009, 2, 1, 16, 35, 23)
+        date_new = datetime.datetime(2009, 2, 5, 17, 38, 30)
         string = date_utils.format(date_old, date_new)
         self.assertEqual(string, "4 days ago")
 
     def test_months(self):
-        date_old = datetime.datetime(2009, 02, 01, 16, 35, 23)
-        date_new = datetime.datetime(2009, 07, 07, 17, 38, 30)
+        date_old = datetime.datetime(2009, 2, 1, 16, 35, 23)
+        date_new = datetime.datetime(2009, 7, 7, 17, 38, 30)
         string = date_utils.format(date_old, date_new)
         self.assertEqual(string, "5 months ago")
 
     def test_years(self):
-        date_old = datetime.datetime(2009, 02, 01, 16, 35, 23)
-        date_new = datetime.datetime(2015, 07, 07, 17, 38, 30)
+        date_old = datetime.datetime(2009, 2, 1, 16, 35, 23)
+        date_new = datetime.datetime(2015, 7, 7, 17, 38, 30)
         string = date_utils.format(date_old, date_new)
         self.assertEqual(string, "6 years ago")
