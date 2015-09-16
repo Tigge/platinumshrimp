af26b4a89d9d00ab5ef76210f58792a96a94d333
Gustav Tiger <gustav@tiger.name>
2015-09-16 14:21:51 +0200

    NBT: Autojoiner
    Changes:
    
    - Moved to Python 3
    - Remove twisted
    - Moved to irc (https://bitbucket.org/jaraco/irc)
    - PEP8 Fixes

--- plugins/autojoiner/autojoiner.py
+++ plugins/autojoiner/autojoiner.py
@@ -5,22 +5,19 @@ import sys
 
 import plugin
 
-from twisted.python import log
-
-
 class Autojoiner(plugin.Plugin):
 
     def __init__(self):
-        plugin.Plugin.__init__(self, "Autojoiner")
+        plugin.Plugin.__init__(self, "autojoiner")
         self.settings = {}
 
     def started(self, settings):
         self.settings = json.loads(settings)
 
-    def onconnected(self, server):
+    def on_welcome(self, server, source, target, message):
         if server in self.settings:
             for channel in self.settings[server]:
-                self.join(server, str(channel))
+                self.join(server, channel)
 
 if __name__ == "__main__":
     sys.exit(Autojoiner.run())
