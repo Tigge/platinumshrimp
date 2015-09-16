07b45f97f1dcb453750469bd50be513bf1b64775
Gustav Tiger <gustav@tiger.name>
2015-09-16 23:36:59 +0200

    NBT: Reconnect

--- bot.py
+++ bot.py
@@ -98,9 +98,16 @@ class Bot:
                       ircname=self.settings['realname'], username=self.settings['username'],
                       connect_factory=factory)
 
+    def reconnect(self, connection):
+        if not connection.is_connected():
+            connection.reconnect()
+
     def _dispatcher(self, connection, event):
         if event.type == "all_raw_messages":
             return
+
+        if event.type == "disconnect":
+            connection.execute_delayed(30, self.reconnect, (connection,))
 
         for plugin in self.plugins:
             plugin._call("on_" + event.type, connection.name, event.source, event.target, *event.arguments)
