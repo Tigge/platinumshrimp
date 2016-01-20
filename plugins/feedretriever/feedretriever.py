Tigge/platinumshrimp

commit b76ba847e4579a9e27ddb2719bec78e8c0271aeb

NBT: Feedretriever
Changes:

- Moved to Python 3
- Remove twisted
- Moved to irc (https://bitbucket.org/jaraco/irc)
- PEP8 Fixes

Additional changes:

Moved to using the `setUpClass` method for getting the test folder
istead of
using a relative path.

Since Feedretriever is the biggest user of utils.command_saver,
utils.file_utils and utils.str_utils these are updated here as well.

plugins/feedretriever/feedretriever.py
@@ -1,7 +1,7 @@
 import feedparser
+import logging
 import sys
-
-from twisted.python import log
+import time
 
 import plugin
 from utils import str_utils, command_saver
@@ -23,28 +23,30 @@ NO_FEED_MESSAGE = u"No feeds"
 
 DEFAULT_FETCH_TIME = 10*60
 
+
 def FeedItemToString(title, link, feed_title = ""):
     return str_utils.sanitize_string(u"{}: {} <{}>".format(feed_title, title, link))
 
+
 # The Feed class handles printing out new entries
-class Feed():
+class Feed:
     # Note that data could both be a url, or an already parsed feed
     def __init__(self, data, title):
-        if isinstance(data, basestring):
+        if isinstance(data, str):
             data = feedparser.parse(data)
-        self.last_entry = 0
+        self.last_entry = time.localtime(0)
         self._set_last(data.entries)
         self.title = title
         self._update_title(data)
 
     def update(self, data, say):
-        if isinstance(data, basestring):
+        if isinstance(data, str):
             data = feedparser.parse(data)
         if data.bozo != 0:
-            log.msg("Error updating feed " + self.title)
+            logging.info("Error updating feed %s", self.title)
             return
         self._update_title(data)
-        log.msg("Updating feed: " + self.title)
+        logging.info("Updating feed: %s", self.title)
         for entry in data.entries:
             # TODO: Check id, title and link, etc
             # Maybe save the entire data.entries and remove all duplicate when
@@ -64,9 +66,9 @@ class Feed():
             self.last_entry = entries[0].published_parsed
 
 
-# Simple polling class, fetches the feed in a regular intervall and passes
+# Simple polling class, fetches the feed in a regular interval and passes
 # the information on to the Feed object
-class Feedpoller():
+class Feedpoller:
     def __init__(self, say, url, update_freq=DEFAULT_FETCH_TIME, title=""):
         parsed = feedparser.parse(url)
         self.feed = Feed(parsed, title)
@@ -98,19 +100,20 @@ class Feedpoller():
                 self.feed.update(parsed, self.say)
                 self.consecutive_fails = 0
 
+
 # Aggregator class for adding and handling feeds
 class Feedretriever(plugin.Plugin):
     def __init__(self):
-        plugin.Plugin.__init__(self, "Feedretriever")
+        plugin.Plugin.__init__(self, "feedretriever")
         self.feeds = []
         self.saver = command_saver.CommandSaver(SAVE_FILE)
 
     def started(self, settings):
-        log.msg("Feedretriever.started", settings)
-        self.saver.read(lambda server, channel, message: self.privmsg(str(server), None, str(channel), message))
+        logging.info("Feedretriever.started %s", settings)
+        self.saver.read(lambda server, channel, message: self.privmsg(server, channel, message))
 
-    def privmsg(self, server, user, channel, message):
-        say = lambda msg: self.say(server, channel, msg)
+    def on_pubmsg(self, server, user, channel, message):
+        say = lambda msg: self.privmsg(server, channel, msg)
         if message.startswith("!feed") or message.startswith("!addfeed"):
             _, url, time, title = str_utils.split(message, " ", 4)
             try:
@@ -125,14 +128,14 @@ class Feedretriever(plugin.Plugin):
         elif message.startswith("!removefeed"):
             feeds = []
             for i in message.split(" "):
-                i = int(i) if unicode(i).isdecimal() else -1
+                i = int(i) if i.isdecimal() else -1
                 if i >= 0 and i < len(self.feeds):
-                    feeds.append(i);
+                    feeds.append(i)
             for i in sorted(feeds, reverse=True):
                 say(REMOVING_FEED_MESSAGE.format(i, self.feeds[i].feed.title))
                 del self.feeds[i]
                 self.saver.remove(i)
-                log.msg("Removed feed: " + str(i))
+                logging.info("Removed feed: %d", i)
         elif message.startswith("!listfeed"):
             if len(self.feeds) == 0:
                 say(NO_FEED_MESSAGE)

plugins/feedretriever/test/test_basic_feed.py
@@ -1,30 +1,39 @@
-from twisted.trial import unittest
+import os
+import unittest
 
 from plugins.feedretriever.feedretriever import Feed, FeedItemToString
+
+
 class FeedRetriverTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.dir = os.path.join("..", os.path.dirname(__file__))
+
     def test_basic_feed(self):
-        with open("../plugins/feedretriever/test/basic_rss_0-entries.xml") as f:
+        with open(os.path.join(self.dir, "basic_rss_0-entries.xml")) as f:
             feed = Feed(f.read(), "Basic Feed")
 
     def test_no_update(self):
-        with open("../plugins/feedretriever/test/basic_rss_0-entries.xml") as f:
+        with open(os.path.join(self.dir, "basic_rss_0-entries.xml")) as f:
             data = f.read()
             feed = Feed(data, "No update")
+
             def say(output):
                 self.fail("Got output even when the feed hasn't been updated: {}".format(output))
             feed.update(data, say)
 
     def test_initial_update(self):
-        with open("../plugins/feedretriever/test/basic_rss_0-entries.xml") as f1:
+        with open(os.path.join(self.dir, "basic_rss_0-entries.xml")) as f1:
             data = f1.read()
             feed_name = "Test"
             feed = Feed(data, feed_name)
             self.updated = False
+
             def say(output):
                 self.assertEqual(output, FeedItemToString("Test Title", "http://www.example.com", feed_name))
                 self.updated = True
-            with open("../plugins/feedretriever/test/basic_rss_1-entries.xml") as f2:
+            with open(os.path.join(self.dir, "basic_rss_1-entries.xml")) as f2:
                 data = f2.read()
                 feed.update(data, say)
                 self.assertTrue(self.updated)
-

plugins/feedretriever/test/test_new_line_input.py
@@ -1,31 +1,39 @@
 from __future__ import unicode_literals
-from twisted.trial import unittest
+import os
+import unittest
 
 from plugins.feedretriever.feedretriever import Feed, FeedItemToString
+
+
 class FeedRetriverTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.dir = os.path.join("..", os.path.dirname(__file__))
+
     def test_title_starting_with_new_line(self):
-        with open("../plugins/feedretriever/test/basic_rss_0-entries.xml") as f:
+        with open(os.path.join(self.dir, "basic_rss_0-entries.xml")) as f:
             feed_name = """
             Test feed"""
             feed = Feed(f.read(), feed_name)
             self.assertEqual(feed.title, "Test feed")
 
     def test_title_ending_with_new_line(self):
-        with open("../plugins/feedretriever/test/basic_rss_0-entries.xml") as f:
+        with open(os.path.join(self.dir, "basic_rss_0-entries.xml")) as f:
             feed_name = """Test feed
             """
             feed = Feed(f.read(), feed_name)
             self.assertEqual(feed.title, "Test feed")
 
     def test_title_has_new_line_in_middle(self):
-        with open("../plugins/feedretriever/test/basic_rss_0-entries.xml") as f:
+        with open(os.path.join(self.dir, "basic_rss_0-entries.xml")) as f:
             feed_name = """Test
             feed"""
             feed = Feed(f.read(), feed_name)
             self.assertEqual(feed.title, "Test feed")
 
     def test_feed_item_has_new_lines(self):
-        with open("../plugins/feedretriever/test/basic_rss_0-entries.xml") as f1:
+        with open(os.path.join(self.dir, "basic_rss_0-entries.xml")) as f1:
             data = f1.read()
             feed_name = """
             Test
@@ -34,11 +42,11 @@ class FeedRetriverTest(unittest.TestCase):
             feed = Feed(data, feed_name)
             self.assertEqual(feed.title, "Test feed")
             self.updated = False
+
             def say(output):
                 self.assertEqual(output, "Test feed: Test Title <http://www.example.com>")
                 self.updated = True
-            with open("../plugins/feedretriever/test/basic_rss_1-entries_new_line.xml") as f2:
+            with open(os.path.join(self.dir, "basic_rss_1-entries_new_line.xml")) as f2:
                 data = f2.read()
                 feed.update(data, say)
                 self.assertTrue(self.updated)
-

utils/command_saver.py
@@ -1,7 +1,6 @@
 import json
 import os
-
-from twisted.python import log
+import logging
 
 # The CommandSaver can be used for saving string parameter lists persistent
 # between bot runs.
@@ -54,10 +53,10 @@ class CommandSaver():
             for line in data:
                 try:
                     callback(*line)
-                except Exception,e:
-                    log.err("Error while reading: " + str(e))
+                except Exception as e:
+                    logging.exception("Error while reading")
         else:
-            log.err("Unable to open file {}, file does not exist".format(self.filename))
+            logging.error("Unable to open file %s, file does not exist", self.filename)
 
     def save(self, *args):
         data = self.read_json(self.filename)
@@ -70,16 +69,16 @@ class CommandSaver():
     def remove_item(self, *args):
         data = self.read_json(self.filename)
         try:
-          data.remove(list(args))
+            data.remove(list(args))
         except:
-          log.err("Can't remove " + str(list(args)) + " from " + str(data))
+            logging.error("Can't remove %s from %s", list(args), data)
         with open(self.filename, 'w+') as file:
             file.write(json.dumps(data))
 
     def remove(self, index):
         data = self.read_json(self.filename)
         if index >= len(data):
-            log.err("Trying to remove something out of index? size: {}, index: {}".format(len(data), index))
+            logging.error("Trying to remove something out of index? size: %d, index: %d", len(data), index)
             return
         del data[index]
         with open(self.filename, 'w+') as file:

utils/test/test_command_saver.py
@@ -1,43 +1,46 @@
 import os
-
-from twisted.trial import unittest
+import unittest
 
 from utils.command_saver import CommandSaver
 
 BASIC_COUNT_CONTENT = "[[0], [1], [2], [3]]"
 DOUBLE_COUNT_CONTENT = "[[0, 1], [1, 2], [2, 3]]"
 
+
 class CommandSaverTest(unittest.TestCase):
     def prepare_test_file(self, content):
         filename = "test_file.save"
         with open(filename, 'w') as f:
-            print>>f, content
+            f.write(content)
         return filename
 
     def verify_content(self, filename, content):
         with open(filename, 'r') as f:
-          self.assertEquals(f.read(), content)
+            self.assertEqual(f.read(), content)
 
     def test_basic_read(self):
         filename = self.prepare_test_file(BASIC_COUNT_CONTENT)
         saver = CommandSaver(filename)
-        # Workaround for changing non-local variable in python 2:
-        index = [0]
+        index = 0
+
         def counter(i):
-            self.assertEquals(index[0], i)
-            index[0] += 1
+            nonlocal index
+            self.assertEqual(index, i)
+            index += 1
         saver.read(counter)
-        self.assertEquals(index[0], 4)
+        self.assertEqual(index, 4)
         self.assertFalse(os.path.isfile(filename))
 
     def test_basic_save(self):
         filename = "basic_save_test_file.save"
+        os.remove(filename)
         saver = CommandSaver(filename)
         saver.save(0)
         saver.save(1)
         saver.save(2)
         saver.save(3)
         self.verify_content(filename, BASIC_COUNT_CONTENT)
+
 
     def test_basic_remove(self):
         filename = self.prepare_test_file(BASIC_COUNT_CONTENT)
@@ -95,14 +98,15 @@ class CommandSaverTest(unittest.TestCase):
     def test_double_read(self):
         filename = self.prepare_test_file(DOUBLE_COUNT_CONTENT)
         saver = CommandSaver(filename)
-        # Workaround for changing non-local variable in python 2:
-        index = [0]
+        index = 0
+
         def counter(i, j):
-            self.assertEquals(index[0], i)
-            self.assertEquals(i + 1, j)
-            index[0] += 1
+            nonlocal index
+            self.assertEqual(index, i)
+            self.assertEqual(i + 1, j)
+            index += 1
         saver.read(counter)
-        self.assertEquals(index[0], 3)
+        self.assertEqual(index, 3)
         self.assertFalse(os.path.isfile(filename))
 
     # Make sure we don't get any half reads when we give a callback with
@@ -110,12 +114,13 @@ class CommandSaverTest(unittest.TestCase):
     def test_double_read_too_few_parameters(self):
         filename = self.prepare_test_file(DOUBLE_COUNT_CONTENT)
         saver = CommandSaver(filename)
-        # Workaround for changing non-local variable in python 2:
-        index = [0]
+        index = 0
+
         def counter(i):
-            index[0] += 1
+            nonlocal index
+            index += 1
         saver.read(counter)
-        self.assertEquals(index[0], 0, "Got read call with too few parameters")
+        self.assertEqual(index, 0, "Got read call with too few parameters")
 
     def test_double_remove(self):
         filename = self.prepare_test_file(DOUBLE_COUNT_CONTENT)
@@ -151,6 +156,7 @@ class CommandSaverTest(unittest.TestCase):
 
     def test_double_save(self):
         filename = "double_save_test_file.save"
+        os.remove(filename)
         saver = CommandSaver(filename)
         saver.save(0, 1)
         saver.save(1, 2)

utils/test/test_file_utils.py
@@ -1,4 +1,4 @@
-from twisted.trial import unittest
+import unittest
 
 from utils import file_utils
 
@@ -12,7 +12,7 @@ class TestStrUtils(unittest.TestCase):
 
     def assert_result(self, file_name, expected_result):
         with open(file_name, 'r') as f:
-            self.assertEquals(f.read().splitlines(), expected_result)
+            self.assertEqual(f.read().splitlines(), expected_result)
 
     def test_remove_first_line(self):
         generate_test_file(TEST_FILE)

utils/test/test_str_utils.py
@@ -1,4 +1,4 @@
-from twisted.trial import unittest
+import unittest
 
 from utils import str_utils
 

commit 74536de646b0bf30d5b252d34ef0ab09b1296f44

Fix Feedretriever issue created by NBT
Regression created in: b76ba847e4579a9e27ddb2719bec78e8c0271aeb

plugins/feedretriever/feedretriever.py
@@ -6,7 +6,7 @@ import time
 import plugin
 from utils import str_utils, command_saver
 
-SAVE_FILE    = "feedretriver_settings.save"
+SAVE_FILE    = "feedretriever_feeds.save"
 FAIL_MESSAGE = ("Unable to download or parse feed.  Remove unused feeds using "
                 "the !listfeed and !removefeed commands.")
 
@@ -110,7 +110,7 @@ class Feedretriever(plugin.Plugin):
 
     def started(self, settings):
         logging.info("Feedretriever.started %s", settings)
-        self.saver.read(lambda server, channel, message: self.privmsg(server, channel, message))
+        self.saver.read(lambda server, channel, message: self.on_pubmsg(server, None, channel, message))
 
     def on_pubmsg(self, server, user, channel, message):
         say = lambda msg: self.privmsg(server, channel, msg)