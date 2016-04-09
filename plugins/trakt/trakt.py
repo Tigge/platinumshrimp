050eb8b00ca84f6845cca1081e5d5f7dbaf9aedf
Gustav Tiger <gustav@tiger.name>
2016-04-09 19:44:59 +0200

    Trakt: Summarize
    Complete the summarize function for trakt.

--- plugins/trakt/trakt.py
+++ plugins/trakt/trakt.py
@@ -55,63 +55,98 @@ class Trakt(plugin.Plugin):
 
         for typ in ["episodes", "movies"]:
 
-            def is_new_item(item):
+            def is_new_item(item, typ=typ):
                 return api.Trakt.get_date(item["watched_at"]) > user["last_sync_" + typ]
 
-            activities = self.fetch_new_activities(user, typ, is_new_item)
+            try:
+                activities = self.fetch_new_activities(username, typ, is_new_item)
 
-            # Continue if we have no entries
-            if len(activities) == 0:
-                continue
+                # Continue if we have no entries
+                if len(activities) == 0:
+                    continue
 
-            for activity in activities:
-                self.users[username]["last_sync_" + typ] = max(self.users[username]["last_sync_" + typ],
-                                                               api.Trakt.get_date(activity["watched_at"]))
+                # Update last sync
+                for activity in activities:
+                    user["last_sync_" + typ] = max(user["last_sync_" + typ],
+                                                   api.Trakt.get_date(activity["watched_at"]))
 
-            activity_summary = self.create_activity_summary(activities)
+                if typ == "episodes":
+                    activity_summary = self.create_activity_summary(activities)
+                    for activity in activity_summary:
+                        message = Trakt.format_activity(activity, username, activity["action"])
+                        if message is not None:
+                            self.echo(message)
+                else:
+                    for activity in activities:
+                        self.echo(Trakt.format_activity(activity, username, activity["action"]))
 
-            #print("got activites2 %s" % activity_summary)
-
-            for entry in activity_summary:
-                print("loop", entry)
-                for series in entry["series"]:
-                    print("get message", series)
-                    message = Trakt.format_activity(series, username, entry["action"])
-                    print("message %s" % message)
-                    if message is not None:
-                        print("echo function %s" % self.echo)
-                        self.echo(message)
-
-            #except Exception as e:
-            #    print("EXCEPTION", repr(e), sys.exc_info()[2].tb_lineno)
-            #    logging.exception("Unhandled exception when fetching for %s of type %s", user, typ)
+            except Exception as e:
+                logging.exception("Unhandled exception when fetching for %s of type %s", user, typ)
 
     def fetch_new_activities(self, user, typ, is_new_item):
         return list(self.trakt.users_history(user, typ, is_new_item))
 
     def create_activity_summary(self, activities):
+        """
+        Returns an activity summary
+        {
+          "action": "scrobble",
+          "show": {...}
+          "seasons": {
+            2: {
+              "number": 2,
+              "ids": {...}
+                "trakt": 1,
+                [...]
+              },
+              "episodes": {
+                5: {
+                  "season": 2,
+                  "number": 5,
+                  "title": "Episode Title",
+                  "ids": {
+                    "trakt": 2,
+                    [...]
+                  }
+                }
+              }
+            }
+          }
+        }
+
+        :param activities:
+        :return:
+        """
         result = {}
         for activity in activities:
-            print("aqqs %s" % activity)
             key = "%s_%s" % (activity["action"], activity["show"]["title"])
             if key not in result:
                 result[key] = {
-                    "show": activity["show"]["title"],
+                    "show": activity["show"],
                     "action": activity["action"],
-                    "episodes": (),
+                    "seasons": {}
                 }
+                for season in self.trakt.seasons_summary(activity["show"]["ids"]["trakt"], extended="full"):
+                    result[key]["seasons"][season["number"]] = season
+                    result[key]["seasons"][season["number"]]["episodes"] = {}
 
-        return [ value for (key, value) in result.items() ]
+            season_number = activity["episode"]["season"]
+            episode_number = activity["episode"]["number"]
+            result[key]["seasons"][season_number]["episodes"][episode_number] = activity["episode"]
+
+        return [value for (key, value) in result.items()]
 
     @staticmethod
-    def format_activity(activity={}, userName="", action=""):
-        return "{0} {1} {2} http://www.trakt.tv{3}".format(userName, Trakt.format_action(action),
+    def format_activity(activity={}, username="", action=""):
+        return "{0} {1} {2} http://www.trakt.tv{3}".format(username, Trakt.format_action(action),
                                                            Trakt.format_item(activity), Trakt.format_url(activity))
 
     @staticmethod
     def format_item(item):
         if "movie" in item:
             return Trakt.format_movie(item["movie"])
+        elif "seasons" in item:
+            return Trakt.format_summary(item)
         elif "episode" in item:
             return Trakt.format_episode(item["show"], item["episode"])
         elif "show" in item:
@@ -129,6 +164,41 @@ class Trakt(plugin.Plugin):
     @staticmethod
     def format_movie(movie):
         return "'{0[title]}' ({0[year]})".format(movie)
+
+    @staticmethod
+    def format_summary(summary):
+
+        def find_episode_ranges(season):
+            ranges = []
+            range_test = []
+            for episode in range(1, season["episode_count"]):
+                if episode not in season["episodes"]:
+                    if len(range_test) > 0:
+                        ranges.append(range_test[:])
+                        range_test = []
+                else:
+                    range_test.append(episode)
+            if len(range_test) > 0:
+                ranges.append(range_test[:])
+            return ranges
+
+        episode_count = (0, None)
+        for season in summary["seasons"].values():
+            if len(season["episodes"]) == 0:
+                continue
+            episode_count = (episode_count[0] + len(season["episodes"]), next(iter(season["episodes"].values())))
+            episode_ranges = find_episode_ranges(season)
+
+            strings = []
+            for episode_range in episode_ranges:
+                if len(episode_range) > 1:
+                    strings.append("S{:02d}E{:02d}-E{:02d}".format(season["number"], episode_range[0], episode_range[-1]))
+                else:
+                    strings.append("S{:02d}E{:02d}".format(season["number"], episode_range[0]))
+
+        if episode_count[0] == 1:
+            return Trakt.format_episode(summary["show"], episode_count[1])
+        return Trakt.format_show(summary["show"]) + " " + ", ".join(strings)
 
     @staticmethod
     def format_show(show):
