dbda23f15a31f90788a22e425e41a27d8d3b3e8a
reggna <230158+reggna@users.noreply.github.com>
2025-04-20 19:20:47 -0700

    Update Chrome version used in the Titlegiver plugin from 43 to 135

--- plugins/titlegiver/titlegiver.py
+++ plugins/titlegiver/titlegiver.py
@@ -33,7 +33,7 @@ class Titlegiver(plugin.Plugin):
     MAX_CONTENT_LENGTH = 64 * 1024
     USER_AGENT = (
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/43.0.2357.37 Safari/537.36"
+        "Chrome/135.0.0.0 Safari/537.36"
     )
     MAX_LINE_COUNT = 16
 
